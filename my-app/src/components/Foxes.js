cfa9177dd85bd8651884c4d456ef718cb3b127e3
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-03-03 18:57:20 -0500

    Make sure everything works so far. Component is export correctly and imported to App.js

--- my-app/src/components/Foxes.js
+++ my-app/src/components/Foxes.js
@@ -1,1 +1,7 @@
-const Foxes = ({}) => {};
+import React from 'react';
+
+const Foxes = ({}) => {
+	return <h2> Foxes! </h2>;
+};
+
+export default Foxes;
---
+++ my-app/src/store/actions/index.js
@@ -0,0 +1,2 @@
+// https://randomfox.ca/floof/
+// use the link above to do my axios call and retrieve the data needed for the app.
